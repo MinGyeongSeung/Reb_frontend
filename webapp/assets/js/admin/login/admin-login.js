a83c6a94056c4ae9ff74f7f0abeace8858fe3e1c
YONGMUN01 <rladyd887@gmail.com>
2025-08-07 11:20:55 +0900

    250807 11:20 김용문 id, class 명 수정

--- webapp/assets/js/admin/login/admin-login.js
+++ webapp/assets/js/admin/login/admin-login.js
@@ -1,11 +1,11 @@
-let loginBtn = document.querySelector("#submit");
-let loginWarning = document.querySelector("#login-warning");
+let loginBtn = document.querySelector("#main-login-submit");
+let loginWarning = document.querySelector("#main-login-warning");
 
 
 
 loginBtn.addEventListener('click', () => {
-  const id = document.querySelector("#id").value;
-  const pw = document.querySelector("#pw").value;
+  const id = document.querySelector("#main-login-id").value;
+  const pw = document.querySelector("#main-login-pw").value;
   if (id === 'admin' && pw === 'admin') {
     loginWarning.style.display = "none";
     alert("성공입니다.");
