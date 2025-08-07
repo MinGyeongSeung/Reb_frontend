644f2b820587cb8cd5cef9fe4f966331a0299898
boraJun <qhfk7751@naver.com>
2025-08-07 10:36:59 +0900

    250807 10:36 전보라 mypage-password 완료, personal-info 작업 시작

---
+++ webapp/assets/js/mypage/mypage-password.js
@@ -0,0 +1,20 @@
+const sideMenuBtnClose = document.querySelector(".mypage-sidemenubtn-close");
+const sideMenuBtnOpen = document.querySelector(".mypage-sidemenubtn-open");
+const sideMenu = document.querySelector(".mypage-sidemenumove");
+const buttonPw = document.querySelector(".button-pw");
+const wrongpw = document.querySelector(".wrongpw");
+
+
+sideMenuBtnClose.addEventListener('click',()=>{moveSideMenu("-300px");});
+sideMenuBtnOpen.addEventListener('click',()=>{moveSideMenu("0px");});
+
+function moveSideMenu(left){
+    sideMenu.style.left = left;
+    sideMenu.style.transition = '0.3s ease';
+    
+    sideMenuBtnClose.innerHTML = "&lt;";
+}
+
+function inputPw(){
+    alert("비밀번호 입력");
+}
