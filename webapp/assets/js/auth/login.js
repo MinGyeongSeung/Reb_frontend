a9265e290faa2514f270fed29572d13fbe78165a
jh011228 <jh01122844@gmail.com>
2025-08-09 23:41:30 +0900

    250809 모달 작업중

--- webapp/assets/js/auth/login.js
+++ webapp/assets/js/auth/login.js
@@ -5,6 +5,10 @@ const idModal = document.querySelector(".modal-id-background");
 const idModalEnd = document.querySelector(".modal-id-end-background");
 const goLoginBtn = document.querySelector(".button-modal-find-end");
 
+const findPwModal = document.querySelector(".p-findpw");
+const closeFindPwModal = document.querySelector(".button-pw-x");
+const pwModal = document.querySelector(".modal-pw-background");
+
 findIdModal.addEventListener('click',()=>{
   idModal.style.display = "flex";
 });
@@ -15,4 +19,13 @@ closeFindIdModal.addEventListener('click',()=>{
 
 goLoginBtn.addEventListener('click',()=>{
   idModalEnd.style.display = "none";
+});
+
+
+findPwModal.addEventListener('click',()=>{
+  pwModal.style.display = "flex";
+});
+
+closeFindPwModal.addEventListener('click',()=>{
+  pwModal.style.display = "none";
 });
