a3d40ea4c4092e21cb0aa732e6a544386d21d255
BoraDorii <jbr@dstrict.com>
2025-08-10 18:44:44 +0900

    250810 18:44 전보라 헤더 완료, 푸터 완료, 마이페이지 헤더 추가 완료

--- webapp/assets/js/header-login.js
+++ webapp/assets/js/header-login.js
@@ -1,7 +1,7 @@
 window.addEventListener('DOMContentLoaded', () => {
     //불러오기
     
-    fetch("./../../header-login-br.html")
+    fetch("./../../header-login.html")
         .then(response => response.text())
         .then(data => {
             header = document.querySelector(`#header`);
