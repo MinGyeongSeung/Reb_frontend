67f348e228b1406e0f91af43e083b1d4569cf2d4
YONGMUN01 <rladyd887@gmail.com>
2025-08-10 18:39:54 +0900

    250810 18:40 김용문 관리자 페이지 js 수정

--- webapp/assets/js/admin/member/admin-expert-list.js
+++ webapp/assets/js/admin/member/admin-expert-list.js
@@ -2,11 +2,12 @@ const search = document.querySelector("#main-search-menu");
 const dropdown = document.querySelector("#main-search-basic");
 const chooseMenu = document.querySelector("#main-search-basic>div>p");
 const modalBackground = document.querySelector("#modal-background");
-// const acceptBtn = document.querySelectorAll(".accept");
-// const cancelBtn = document.querySelectorAll(".cancel");
-const cancelBtn = document.querySelector(".cancel");
-const acceptBtn = document.querySelector(".accept");
+const acceptBtn = document.querySelectorAll(".accept");
+const cancelBtn = document.querySelectorAll(".cancel");
+// const cancelBtn = document.querySelector(".cancel");
+// const acceptBtn = document.querySelector(".accept");
 const submitBtn = document.querySelector("#submit");
+const closeModal = document.querySelector("#close-modal");
 
 dropdown.addEventListener('click', () => {
   search.style.display = "block";
@@ -17,28 +18,39 @@ chooseMenu.addEventListener('blur', () => {
 });
 
 
-cancelBtn.addEventListener('click', () => {
-  modalBackground.style.display = "flex";
-  modalBackground.style.zIndex = 5;
+
+
+acceptBtn.forEach(btn => {
+  btn.addEventListener('click', function (e) {
+    const nextBtn = e.target.nextElementSibling;
+    nextBtn.style.width = "90px";
+    btn.style.display = "none";
+    nextBtn.innerText = "승인 취소";
+  });
 });
 
-// for (let i = 0; i < acceptBtn.length; i++) {
-//   acceptBtn.addEventListener('click', () => {
-//     cancelBtn[i].style.width = "90px";
-//     acceptBtn[i].style.display = "none";
-//     cancelBtn[i].style.backgroundColor = "#be0000";
-//     cancelBtn[i].innerText = "승인 취소";
-//   });
-// }
 
-acceptBtn.addEventListener('click', () => {
-    cancelBtn.style.width = "90px";
-    acceptBtn.style.display = "none";
-    cancelBtn.style.backgroundColor = "#be0000";
-    cancelBtn.innerText = "승인 취소";
+cancelBtn.forEach(btn => {
+  btn.addEventListener('click', function (e) {
+    modalBackground.style.display = "flex";
+    modalBackground.style.zIndex = 5;
+    submitBtn.addEventListener('click', () => {
+      alert("취소/반려 사유 등록 완료");
+      // location.href = "./../../../app/admin/member/admin-expert-list.html";
+      modalBackground.style.display = "none";
+      const list= e.target.nextElementSibling.nextElementSibling;
+      list.remove();
+    });
   });
+});
 
-  submitBtn.addEventListener('click', ()=>{
-    alert("취소/반려 사유 등록 완료");
-    location.href= "./../../../app/admin/member/admin-expert-list.html";
-  })
+// submitBtn.addEventListener('click', () => {
+//   alert("취소/반려 사유 등록 완료");
+//   // location.href = "./../../../app/admin/member/admin-expert-list.html";
+//   modalBackground.style.display= "none";
+// });
+
+
+closeModal.addEventListener(('click'), () => {
+  modalBackground.style.display = "none";
+});
