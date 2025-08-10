a4bbfaaa1376ce9619d856bfda7827eca4b0b424
BoraDorii <jbr@dstrict.com>
2025-08-10 16:44:10 +0900

    250810 16:43 전보라 마이페이지 헤더 추가

--- webapp/header-login-br.js
+++ webapp/header-login-br.js
@@ -1,7 +1,7 @@
 window.addEventListener('DOMContentLoaded', () => {
     //불러오기
     
-    fetch("http://localhost:5500/Reb_frontend/webapp/header-login-br.html")
+    fetch("./../../header-login-br.html")
         .then(response => response.text())
         .then(data => {
             modal = document.querySelector(`#header`);
