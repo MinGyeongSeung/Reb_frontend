b89ecc20d2cc65d7cdb33f910a3ffce8020a3e34
YONGMUN01 <rladyd887@gmail.com>
2025-08-11 10:07:13 +0900

    250811 10:07 김용문 배너 삭제 기능 추가

--- webapp/assets/js/admin/banner/admin-banner-list.js
+++ webapp/assets/js/admin/banner/admin-banner-list.js
@@ -1,17 +1,21 @@
-const deleteBtn = document.querySelector(".delete");
 const addBtn = document.querySelector("#main-banner-add");
+const deleteBtn = document.querySelectorAll(".delete");
 
-deleteBtn.addEventListener('click', ()=>{
-  if(confirm("정말로 삭제하시겠습니까?")){
-    alert("삭제 완료되었습니다.");
-  }
-});
-
-addBtn.addEventListener('click', ()=>{
-  location.href="./../../../app/admin/banner/admin-banner-create.html";
+addBtn.addEventListener('click', () => {
+  location.href = "./../../../app/admin/banner/admin-banner-create.html";
 });
 
 const logoutBtn = document.querySelector("header>button>img");
-logoutBtn.addEventListener('click', ()=>{
-  location.href= "./../login/admin-login.html";
+logoutBtn.addEventListener('click', () => {
+  location.href = "./../login/admin-login.html";
+});
+
+deleteBtn.forEach(btn => {
+  btn.addEventListener('click', function (e) {
+    if (confirm("정말로 삭제하시겠습니까?")) {
+      alert("삭제 완료되었습니다.");
+      const list = e.target.parentNode.parentNode.parentNode;
+      list.remove();
+    };
+  });
 });
