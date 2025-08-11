ddcfd2499a3a5d6de4a03b66357a8a0326b24671
boraJun <qhfk7751@naver.com>
2025-08-11 11:01:15 +0900

    250811 11:00 후원 페이지 css 수정, 헤더 푸터 수정 중

--- webapp/assets/js/footer.js
+++ webapp/assets/js/footer.js
@@ -1,5 +1,14 @@
 window.addEventListener('DOMContentLoaded', () => {
-    fetch("./../../footer.html")
+    var xhr = new XMLHttpRequest();
+    let path = "./footer.html";
+    xhr.open('HEAD', path, false);
+    xhr.send();
+
+    if (xhr.status == "404") {
+      path = "./../."+ path;
+    } 
+
+    fetch(path)
         .then(response => response.text())
         .then(data => {
             footer = document.querySelector(`#footer`);
