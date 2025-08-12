f50d5fea6de2dfa3152df71dffe1022ee86fe2b7
jh011228 <jh01122844@gmail.com>
2025-08-12 12:33:04 +0900

    250812 12:32 최장현 루틴모임 상세->삭제 주소 재설정

--- webapp/assets/js/routine-meeting/routine-meeting-review-detail.js
+++ webapp/assets/js/routine-meeting/routine-meeting-review-detail.js
@@ -9,7 +9,7 @@ back.addEventListener("click",()=>{
 deleteButton.addEventListener("click",(e)=>{
   if(confirm("정말 삭제하시겠습니까?")){
     alert("삭제 완료");
-    location.href = "./course-review-list.html";
+    location.href = "./routine-meeting-review-list.html";
   }
   else{
     e.preventDefault();
